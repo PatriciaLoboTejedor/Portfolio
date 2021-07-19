bb9e32fcd2f4c1fd7aaa2592e7b605a9d1a17246
PatriciaLoboTejedor <patricialobotejedor@gmail.com>
2021-07-20 00:15:21 +0200

    change menu in mobile version

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+console.log(">> Ready :)");
+
+// selector
+var menu = document.querySelector(".js-hamburger");
+
+// method
+function toggleMenu(event) {
+  this.classList.toggle("is-active");
+  document.querySelector(".js-nav").classList.toggle("is_active");
+  event.preventDefault();
+}
+
+// event
+menu.addEventListener("click", toggleMenu, false);
